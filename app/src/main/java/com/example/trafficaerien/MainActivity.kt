78309556b2213228ddb8d022d7bd4c1340c8e8cd
Camagny <robin2b2010@gmail.com>
2020-11-19 12:12:44 +0100

    get in log what is selected

--- app/src/main/java/com/example/trafficaerien/MainActivity.kt
+++ app/src/main/java/com/example/trafficaerien/MainActivity.kt
@@ -3,15 +3,13 @@ package com.example.trafficaerien
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.JsonArray
+import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
-
 
 class MainActivity : AppCompatActivity() {
     val DATE_FORMAT = "dd MMM yyy"
@@ -19,21 +17,17 @@ class MainActivity : AppCompatActivity() {
     val fromCalendar = Calendar.getInstance()
     val toCalendar = Calendar.getInstance()
 
+    var airportList = Utils.generateAirportList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val textView     = findViewById<TextView>(R.id.editDate)
-        val textView2     = findViewById<TextView>(R.id.editDate2)
-        val spinner: Spinner = findViewById(R.id.spinner)
-
-        var airports = Utils.generateAirportList()
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         val listAirportName: ArrayList<String> = ArrayList<String>()
 
         //add name airport in listAirportName
-        for(airport in airports){
+        for(airport in airportList){
             listAirportName.add(airport.getFormattedName())
         }
 
@@ -46,15 +40,19 @@ class MainActivity : AppCompatActivity() {
 
         spinner.adapter=adapter
 
-        displaySelectedDate(textView, fromCalendar)
-        displaySelectedDate(textView2, toCalendar)
+        displaySelectedDate(editDate, fromCalendar)
+        displaySelectedDate(editDate2, toCalendar)
 
-        textView.setOnClickListener {
+        editDate.setOnClickListener {
             showDatePicker(textView, fromCalendar)
         }
 
-        textView2.setOnClickListener {
-            showDatePicker(textView2, toCalendar)
+        editDate2.setOnClickListener {
+            showDatePicker(editDate2, toCalendar)
+        }
+
+        searchButton.setOnClickListener {
+            search()
         }
     }
 
@@ -80,5 +78,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun displaySelectedDate(textView: TextView, c : Calendar){
         textView.text = SimpleDateFormat(DATE_FORMAT, Locale.getDefault()).format(c.time)
+    }
+
+    private fun search(){
+        //récupérer aeroport
+        val icao = airportList.get(spinner.selectedItemPosition)
+
+        //récupérer isArrival
+        val isArrival = radioButton2.isActivated
+
+        //récupérer les 2 dates
+        val begin = fromCalendar.timeInMillis / 1000
+        val end = toCalendar.timeInMillis / 1000
+
+        Log.d("MainActivity", "icao=$icao isArrival=$isArrival begin=$begin end=$end ")
     }
 }
